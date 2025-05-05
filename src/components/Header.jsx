ee746428005d6fa01630043b99f26ba2fe5342a4
Damian <damian.pillay@kcpivot.co.za>
2025-05-05 10:51:53 +0200

    Squash merge feature/header into main

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import Logo from "../assets/checkbox.png";
+
+export default function Header() {
+  return (
+    <header className="pt-20 sm:pt-32 lg:pt-40 text-8xl">
+      <h1 className="font-black text-center flex items-center justify-center gap-2 sm:gap-1 lg:gap-3 text-4xl sm:text-6xl lg:text-8xl">
+        <span>
+          <img
+            src={Logo}
+            className="h-[1em] w-[1em] object-contain mr-2 sm:mr-4 lg:mr-5"
+          />
+        </span>
+        <span className="text-violet-400">To-Do</span> List
+      </h1>
+    </header>
+  );
+}
