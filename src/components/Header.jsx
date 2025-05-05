b3f4b1acb479dea2d611d6c5565d443acfcee041
Damian <damian.pillay@kcpivot.co.za>
2025-05-05 14:00:59 +0200

    Squash merge hotfix/header-margin-adjustment into main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import Logo from "../assets/checkbox.png";
 
 export default function Header() {
   return (
-    <header className="pt-20 sm:pt-32 lg:pt-40 text-8xl">
-      <h1 className="font-black text-center flex items-center justify-center gap-2 sm:gap-1 lg:gap-3 text-4xl sm:text-6xl lg:text-8xl">
+    <header className="pt-20 sm:pt-32 lg:pt-40 text-8xl mb-8">
+      <h1 className="font-black text-center flex items-center justify-center gap-2 sm:gap-1 lg:gap-3 text-4xl sm:text-7xl lg:text-8xl">
         <span>
           <img
             src={Logo}
