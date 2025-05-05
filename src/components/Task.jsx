8e231a31f7c9891147f9c193726f61971e8d91d3
Damian <damian.pillay@kcpivot.co.za>
2025-05-05 13:57:40 +0200

    refactor: move tailwind css within the elements they correspond to

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -8,20 +8,17 @@ export default function Task({ children, index }) {
     console.log(isChecked);
   }
 
-  const listStyling = "mb-4 w-158 rounded-2xl bg-neutral-900";
-  const labelStyling =
-    "flex p-7 gap-7 w-full rounded-2xl cursor-pointer sm:text-1xl lg:text-lg";
-  const checkboxStyling =
-    "appearance-none h-6 w-6 border-2 border-violet-400 rounded-lg checked:bg-violet-400 checked:border-transparent focus:outline-none";
-
   return (
-    <li key={index} className={listStyling}>
-      <label className={labelStyling}>
+    <li
+      key={index}
+      className="mb-5 ml-8 mr-8 lg:w-230 sm:w-150 rounded-2xl bg-neutral-900"
+    >
+      <label className="flex p-[1.8em] gap-7 w-full rounded-2xl cursor-pointer text-sm sm:text-1xl lg:text-2xl">
         <input
           type="checkbox"
           checked={isChecked}
           onChange={handleChange}
-          className={checkboxStyling}
+          className="appearance-none h-6 w-6 border-2 border-violet-400 rounded-lg checked:bg-violet-400 checked:border-transparent focus:outline-none"
         />
         <span
           className={`select-none ${
