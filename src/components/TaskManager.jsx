fc443ceb91e08ef77c7c3ac1271c83c5ebf75885
Damian <damian.pillay@kcpivot.co.za>
2025-05-06 10:03:36 +0200

    feat: change task structure to now use objects instead of arrays

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -3,22 +3,24 @@ import Task from "./Task";
 import { useState } from "react";
 
 const dummyTask = [
-  "Create Dark mode feature",
-  "Create add task feature",
-  "Create search feature",
+  { id: 1, text: "Create Dark mode feature" },
+  { id: 2, text: "Create add task feature" },
+  { id: 3, text: "Create search feature" },
 ];
 
 export default function TaskManager() {
   const [taskList, setTaskList] = useState([...dummyTask]);
 
   function handleClick() {
-    setTaskList((prevList) => ["", ...prevList]);
-    console.log(taskList);
+    const newTask = { id: Date.now(), text: "" };
+    setTaskList((prevList) => [newTask, ...prevList]);
   }
 
-  function handleChange(index, newTask) {
+  function handleChange(id, newTaskText) {
     setTaskList((prevList) =>
-      prevList.map((task, i) => (i === index ? newTask : task))
+      prevList.map((task) =>
+        task.id === id ? { ...task, text: newTaskText } : task
+      )
     );
   }
 
@@ -26,13 +28,13 @@ export default function TaskManager() {
     <>
       <div className="flex justify-center">
         <ul>
-          {taskList.map((task, index) => (
+          {taskList.map((task) => (
             <Task
-              index={index}
+              id={task.id}
               taskList={taskList}
               handleInputConfirmation={handleChange}
             >
-              {task}
+              {task.text}
             </Task>
           ))}
         </ul>
