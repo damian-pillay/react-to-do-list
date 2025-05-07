1539aeceff69403fb63d044ff3b59f53bb93d068
Damian <damian.pillay@kcpivot.co.za>
2025-05-07 11:13:02 +0200

    fix: make the logo none selectable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Logo from "../assets/checkbox.png";
 
 export default function Header() {
   return (
-    <header className="pt-20 sm:pt-32 lg:pt-40 text-8xl mb-8">
+    <header className="pt-20 sm:pt-32 lg:pt-40 text-8xl mb-8 select-none">
       <h1 className="font-black text-center flex items-center justify-center gap-2 sm:gap-1 lg:gap-3 text-4xl sm:text-7xl lg:text-8xl">
         <span>
           <img
